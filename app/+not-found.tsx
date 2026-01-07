6715642a3c27078b181e838acc02fce3e73c0a79
parulpragya767 <parulpragya767@gmail.com>
2026-01-07 11:55:48 +0530

    not found screen

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,30 +1,24 @@
-import { ThemedView } from '@/src/components/themes/themed-view';
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Button } from '@/src/components/ui/Button';
+import { Screen } from '@/src/components/ui/Screen';
+import { DefaultHeaderOptions } from '@/src/components/ui/navigation/HeaderOptions';
+import { Stack, useRouter } from 'expo-router';
 
 export default function NotFoundScreen() {
+  const router = useRouter();
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops! Not Found' }} />
-      <ThemedView style={styles.container}>
-        <Link href="/" style={styles.button}>
+    <Stack.Screen 
+      options={{
+        title: 'Oops! Not Found',
+        gestureEnabled: false,
+        ...DefaultHeaderOptions,
+      }}
+    />
+      <Screen className="flex-1 items-center justify-center">
+        <Button onPress={() => router.replace('/')}>
           Go back to Home!
-        </Link>
-      </ThemedView>
+        </Button>
+      </Screen>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
-  },
-});
