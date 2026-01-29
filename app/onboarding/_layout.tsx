22c074f4d7e3a6d3b79865f9ab5262ad866203c2
parulpragya767 <parulpragya767@gmail.com>
2026-01-29 16:06:37 +0530

    fix onboarding routing issues

--- app/onboarding/_layout.tsx
+++ app/onboarding/_layout.tsx
@@ -1,19 +1,15 @@
 import LoadingState from '@/src/components/states/LoadingState';
 import { useAuth } from '@/src/context/AuthContext';
-import { useUserStore } from '@/src/store/useUserStore';
 import { Redirect, Stack } from 'expo-router';
 import React from 'react';
 
 export default function OnboardingLayout() {
   const { loading, sessionUser: user } = useAuth();
-  const { onboardingCompleted } = useUserStore();
 
   if (loading) return <LoadingState text="Loading your profile..." />;
 
   if (!user) return <Redirect href="/auth" />;
 
-  if (onboardingCompleted) return <Redirect href="/" />;
-    
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
